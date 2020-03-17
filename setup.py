a33890497ea6c1ea7b23a4f3e8d599821973754d
JasonArmitage-res <51964049+JasonArmitage-res@users.noreply.github.com>
2020-03-17 20:38:54 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="PM_MO",
     version="1.0.0",
-    author="Shramana Thakur , Rishi Tripathi",
-    author_email="s6shthak@uni-bonn.de , s6ritrip@uni-bonn.de",
+    author="Jason Armitage, Shramana Thakur , Rishi Tripathi",
+    author_email="jason.armitage@uni-bonn.de, s6shthak@uni-bonn.de , s6ritrip@uni-bonn.de",
     description="",
     long_description=long_description,
     long_description_content_type="text/markdown",
